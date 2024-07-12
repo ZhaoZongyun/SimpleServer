c2fcb78d3a16192e684191a8de06a0f775b0a4bd
zhaozongyun <fuyunzzy@163.com>
2024-07-12 13:21:31 +0800

    首次提交

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using ProtobufGen;
+namespace TestUDPServer
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            NetMgr netMgr = new NetMgr();
+            netMgr.Init();
+        }
+    }
+}
